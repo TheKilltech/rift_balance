1f5cfacb05594fa03bad8ec1b24249eb49016eea
Killtech <killtec@hotmail.com>
2023-07-01 19:32:03 +0200

    testing better difficulty progression on acid

--- lua/missions/campaigns/story/v2/acid/dom_acid_find_rare_resource_rules_custom.lua
+++ lua/missions/campaigns/story/v2/acid/dom_acid_find_rare_resource_rules_custom.lua
@@ -5,26 +5,26 @@ return function()
 	local rulesName = GetRulesForCustomDifficulty( "lua/missions/campaigns/story/v2/acid/dom_acid_find_rare_resource_rules_" )
 	rules = require( rulesName )()	
 
-	local attackCountMultiplier			  = DifficultyService:GetAttacksCountMultiplier()
-	local prepareAttackTimeMultiplier     = DifficultyService:GetPrepareAttackTimeMultiplier()
-	local idleTimeMultiplier			  = DifficultyService:IdleTimeMultiplier()
-	local buildingSpeedMultiplier 		  = DifficultyService:GetBuildingSpeedMultiplier()
-	local difficultyProgressionMultiplier = math.sqrt(buildingSpeedMultiplier)
+	local attackCountMultiplier			= DifficultyService:GetAttacksCountMultiplier()
+	local prepareAttackTimeMultiplier   = DifficultyService:GetPrepareAttackTimeMultiplier()
+	local idleTimeMultiplier			= DifficultyService:IdleTimeMultiplier()
+	local buildingSpeedMultiplier 		= DifficultyService:GetBuildingSpeedMultiplier()
+	local progressionMultiplier 		= math.sqrt(buildingSpeedMultiplier) -- this should be a product of building speed and building cost multipliers. the squareroot is used because the overall gameplay-progress speed depends on more then just those two apsects
 
 	for i = 1, #rules.maxAttackCountPerDifficulty, 1 do
 		rules.maxAttackCountPerDifficulty[i] = rules.maxAttackCountPerDifficulty[i] * attackCountMultiplier
 	end
 
 	for i = 1, #rules.idleTime, 1 do
-		rules.idleTime[i] = rules.idleTime[i] * idleTimeMultiplier
+		rules.idleTime[i] = rules.idleTime[i] * idleTimeMultiplier * (1 + (progressionMultiplier-1)*(9-i)/8.0)
 	end
 
 	for i = 1, #rules.prepareSpawnTime, 1 do
-		rules.prepareSpawnTime[i] = rules.prepareSpawnTime[i] * prepareAttackTimeMultiplier
+		rules.prepareSpawnTime[i] = rules.prepareSpawnTime[i] * prepareAttackTimeMultiplier * (1 + (progressionMultiplier-1)*(9-i)/8.0)
 	end
 	
 	for i = 1, #rules.timeToNextDifficultyLevel, 1 do
-		rules.timeToNextDifficultyLevel[i] = rules.timeToNextDifficultyLevel[i] * difficultyProgressionMultiplier
+		rules.timeToNextDifficultyLevel[i] = rules.timeToNextDifficultyLevel[i] * (1 + (progressionMultiplier-1)*(9-i)/8.0)
 	end
 
     return rules;
--- lua/missions/campaigns/story/v2/acid/dom_acid_find_rare_resource_rules_default.lua
+++ lua/missions/campaigns/story/v2/acid/dom_acid_find_rare_resource_rules_default.lua
@@ -65,15 +65,15 @@ return function()
 
 	rules.timeToNextDifficultyLevel = 
 	{			
-		120, -- difficulty level 1
-		120, -- difficulty level 2
-		120, -- difficulty level 3
+		180, -- difficulty level 1
+		180, -- difficulty level 2
+		180, -- difficulty level 3
 		300, -- difficulty level 4
-		600, -- difficulty level 5
-		600, -- difficulty level 6
-		780, -- difficulty level 7
-		780, -- difficulty level 8
-		780, -- difficulty level 9
+		900, -- difficulty level 5
+		1020, -- difficulty level 6
+		1200, -- difficulty level 7
+		1500, -- difficulty level 8
+		1500, -- difficulty level 9
 	}
 
 	rules.prepareSpawnTime = 
