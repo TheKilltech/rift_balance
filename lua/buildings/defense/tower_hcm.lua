c2d99bef302e9e316fe524a33d7092c124c517b9
Killtech <killtec@hotmail.com>
2026-04-04 22:28:31 +0200

    new lua for HCM

--- lua/buildings/defense/tower_hcm.lua
+++ lua/buildings/defense/tower_hcm.lua
@@ -52,6 +52,9 @@ function tower_hcm:OnDeactivate()
 	end
 end
 
+function tower_hcm:_OnTurretEvent( evt )
+end
+
 function tower_hcm:OnTurretEvent( evt )
    self:UpdateMuzzles()
 end
@@ -59,6 +62,7 @@ end
 function tower_hcm:OnWorkingEnter( state )
 	if self.pendingRocket == nil and self.readyRocker == nil then
 		self.pendingRocket = EntityService:SpawnAndAttachEntity( self.rocketBp, self.entity, "att_rocket", "" )
+		BuildingService:RemoveConverterCostModifier( self.entity, "idle" )
 	end
 end
 
@@ -104,19 +108,30 @@ function tower_hcm:OnWorkingExecute( state )
 				end
 			end
 
+			BuildingService:RemoveConverterCostModifier( self.entity, "idle" )
 			self.pendingRocket = EntityService:SpawnAndAttachEntity( self.rocketBp, self.entity, "att_rocket", "" )
 			self.fsm:ChangeState( "cooldown" )
 		end
 	end
 
 	if self.pendingRocket ~= nil then
+		local statusInfo = ""
 		self.data:SetInt("is_ready", 1 )
 		if progress > 0 and progress < 1.0 then
+			statusInfo = string.format("%.0f", (progress)*100) .. "%"
 	    	EntityService:SetPosition( self.pendingRocket, self.rocketInitialHeight * progress, 0, 0 )
+			
 		elseif progress >= 1 then
+			statusInfo = "ready"
 	    	self.readyRocker = self.pendingRocket;
 	    	self.pendingRocket = nil
+			BuildingService:AddConverterCostModifier( self.entity, 0 , "idle" )
 		end
+		self.data:SetString("production_group.rows.row1.icon", "gui/hud/buttons/action_menu_upgrade_neutral")
+		self.data:SetString("production_group.rows.row1.name", "Rocket Prep" )
+		self.data:SetString("production_group.rows.row1.value", statusInfo )
+		self.data:SetString("stat_categories", "production_group")
+		self.data:SetString("production_group.rows", "row1" )
 	end
 
 	self:UpdateMuzzles()
@@ -136,4 +151,5 @@ function tower_hcm:DetachAndFireRocket( rocketEnt )
 	QueueEvent( "DissolveEntityRequest", rocketEnt, 2.0, 3.0 )
 	EffectService:AttachEffects( rocketEnt, "exhaust" )
 end
+
 return tower_hcm
