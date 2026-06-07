61203644a42069c9736e46d54ddab7f995a7a2ae
Killtech <killtec@hotmail.com>
2026-06-07 19:12:52 +0200

    finalized investigate bioanomaly logic

--- lua/graph/entity/entity_spawn_single.lua
+++ lua/graph/entity/entity_spawn_single.lua
@@ -6,29 +6,18 @@ function entity_spawn_single:__init()
     LuaGraphNode.__init(self, self)
 end
 
-function entity_spawn_single:init()	
-	self.blueprint = self.data:GetString( "blueprint" )	
-	self.team = self.data:GetString( "team" )		
-	
-	self.entityName = self.data:GetString( "entity_name" )
+function entity_spawn_single:init()
+	self.entityGroup = self.data:GetString( "entity_group" )
+	self.entityName  = self.data:GetString( "entity_name" )
 	if self.entityName ~= "" and self.data:GetIntOrDefault("name_is_global", 1) == 0 then
 		self.entityName = self.parent:CreateGraphUniqueString(self.entityName)
 	end
 
-	self.entityGroup = self.data:GetString( "entity_group" )	
-	
-	self.singleTarget = self.data:GetIntOrDefault("target_single", 0)
-	self.targetAttachment = self.data:GetStringOrDefault( "target_attachment", "" )
-
-	self.searchTargetType = self.data:GetString("search_target_type")
+	self.searchTargetType  = self.data:GetString("search_target_type")
     self.searchTargetValue = self.data:GetString("search_target_value")
 
-    self.targetFindType = self.data:GetString("find_type") 
-	self.targetFindValue = self.data:GetString("find_value") 
-	
-	self.searchRadius = self.data:GetFloatOrDefault("search_radius", 0)
-
-	self.attach = self.data:GetIntOrDefault("attach_entity", 0)
+    self.targetFindType    = self.data:GetString("find_type") 
+	self.targetFindValue   = self.data:GetString("find_value") 
 	
     if self.searchTargetType == "LocalName" then
 		self.searchTargetType = "Name"
@@ -39,42 +28,61 @@ function entity_spawn_single:init()
 		self.targetFindType = "Name"
 		self.targetFindValue = self.parent:CreateGraphUniqueString(self.targetFindValue)
 	end
+	LogService:Log( "spawn entity: init - " .. tostring(self.entityName) .. " " .. tostring(self.entityGroup))
 end
 
-function entity_spawn_single:Activated()	
-    self.entities = FindEntitiesByTarget(self.targetFindType, self.targetFindValue, 0.0, self.searchRadius, self.searchTargetType, self.searchTargetValue);
+function entity_spawn_single:Activated()
+	local blueprint  = self.data:GetString( "blueprint" )
+	local team       = self.data:GetString( "team" )
+	local minTime    = self.data:GetIntOrDefault("min_life_time", 0)
+	local maxTime    = self.data:GetIntOrDefault("max_life_time", 0)
+	local lifeTime   = math.random( minTime, maxTime )
+
+	local targetAttachment  = self.data:GetStringOrDefault( "target_attachment", "" )
+	local attach            = self.data:GetIntOrDefault("attach_entity", 0)
+	local singleTarget      = self.data:GetIntOrDefault("target_single", 0)
+	local searchRadius      = self.data:GetFloatOrDefault("search_radius", 0)
+	
+	LogService:Log( "spawn entity: Activated - " .. tostring(blueprint) .. " " .. tostring(self.entityName) .. " " .. tostring(self.entityGroup))
+	
+	self.entities = FindEntitiesByTarget(self.targetFindType, self.targetFindValue, 0.0, searchRadius, self.searchTargetType, self.searchTargetValue);
 	if ( #self.entities == 0 ) then
+		LogService:Log( "spawn entity: failed (target not found)")
 		Assert( self.entities ~= 0, "ERROR: Spawn Entity block failed to find a target - skipping block" )
 		self:SetFinished()
 		return
 	end
 	for entity in Iter(self.entities) do
 		-- Spawn entity
-		if self.targetAttachment == "" then
-			self.entityId = EntityService:SpawnEntity( self.blueprint, entity, self.team )
+		if targetAttachment == "" then
+			self.entityId = EntityService:SpawnEntity( blueprint, entity, team )
 		else
-			self.entityId = EntityService:SpawnEntity( self.blueprint, entity, self.targetAttachment, self.team )
+			self.entityId = EntityService:SpawnEntity( blueprint, entity, targetAttachment, team )
 		end
 		-- Name the entity, but only if it is singular
-		if ( entityName ~= "" ) then
-			EntityService:SetName( self.entityId, self.entityName )			
+		if ( self.entityName ~= "" ) then
+			EntityService:SetName( self.entityId, self.entityName )
 		end
 	
-		if entityGroup ~= "" then
-			EntityService:SetGroup( self.entityId, self.entityGroup )		
+		if ( self.entityGroup ~= "" ) then
+			EntityService:SetGroup( self.entityId, self.entityGroup )
 		end	
 
-		if ( self.attach == 1 ) then
-			if self.targetAttachment == "" then
+		if lifeTime > 0 then
+			EntityService:CreateLifeTime( self.entityId, lifeTime, "" )
+		end
+		
+		if ( attach == 1 ) then
+			if targetAttachment == "" then
 				EntityService:AttachEntity( self.entityId, entity)
 			else
-				EntityService:AttachEntity( self.entityId, entity,self.targetAttachment)
+				EntityService:AttachEntity( self.entityId, entity, targetAttachment)
 			end
 			EntityService:SetPosition( self.entityId, 0, 0, 0)
 		end
 		
 		-- If SIngle Entity Switch is enabled then break the loop here
-		if self.singleTarget == 1 then
+		if singleTarget == 1 then
 			self:SetFinished()
 			return
 		end
